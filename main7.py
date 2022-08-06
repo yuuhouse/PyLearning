e361d2a0c48873b0fb82b3f17a531489dea89ec6
yuuhouse <44646567+yuuhouse@users.noreply.github.com>
2022-08-06 18:07:44 +0800

    Update main7.py

--- main7.py
+++ main7.py
@@ -1,7 +1,15 @@
 # 資料型態
 # string
+'你好'
+print('你好')
 # int
+300
+100+300
+print(100)
+print(100+300)  # 可以直接相加數字
 # float
-# boolean
-
- 
+160.5
+print(160.66666666)
+# boolean #注意開頭要大寫
+True
+False
