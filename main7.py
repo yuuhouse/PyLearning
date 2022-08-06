6c0e132a1678413a5a799bb99ba1c9d6b2319c5c
yuuhouse <44646567+yuuhouse@users.noreply.github.com>
2022-08-07 01:33:11 +0800

    資料型態轉換

--- main7.py
+++ main7.py
@@ -2,14 +2,20 @@
 # string
 '你好'
 print('你好')
+name = "你好小白"
+print("你好小白"[0])
+print(name[3])  # 抓取特定位置字元
 # int
 300
-100+300
+100 + 300
+
 print(100)
-print(100+300)  # 可以直接相加數字
+print(100 + 300)  # 可以直接相加數字
+print(1_000_000)  # 數字可以加底線
 # float
-160.5
+num = 160.5
 print(160.66666666)
+print(num)
 # boolean #注意開頭要大寫
 True
 False
---
+++ main8.py
@@ -0,0 +1,23 @@
+# 轉換資料型態
+
+num1 = input("plz num1:")
+num2 = input("plz num2:")
+print(num1 + num2)
+print(type(num1))  # 查詢資料型態
+print(type(num2))
+
+int()  # 轉換成int
+float()  # 轉換float
+
+num3 = int(input("plz num3:"))  # 轉換成int
+num4 = int(input("plz num4:"))
+print(num3 + num4)  # int相加
+
+num5 = float(input("plz num5:"))  # 轉換成float
+num6 = float(input("plz num6:"))
+print(num5 + num6)  # float相加
+
+num7 = 10
+num8 = "20"
+print(str(num7)+num8)  # 改成字串是str
+
