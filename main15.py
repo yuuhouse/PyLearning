1757a28b63f272851fe0c5669b6847d51de75818
yuuhouse <44646567+yuuhouse@users.noreply.github.com>
2022-08-07 17:15:58 +0800

    elif , nested if(巢狀

---
+++ main15.py
@@ -0,0 +1,17 @@
+# elif , nested if(巢狀
+
+score = int(input("score:"))
+if score >= 90 or score == 100:
+    print("甲")
+    if score == 100:
+        print("最頂的")
+    else:
+        print("你怎麼沒有考100")
+elif score >= 80:
+    print("乙")
+elif score >= 70:
+    print("丙")
+elif score >= 60:
+    print("丁")
+else:
+    print("不及格")
