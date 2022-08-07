3b3984016b899fa9bb5463a91b847cdf1d8725db
yuuhouse <44646567+yuuhouse@users.noreply.github.com>
2022-08-07 17:31:12 +0800

    # 拉麵點餐系統

---
+++ main16.py
@@ -0,0 +1,10 @@
+# 拉麵點餐系統
+print("welcome ramen order\n "
+      "1.salt ramen 220\n "
+      "2.soy sauce ramen 240\n"
+      "3.tonkotsu ramen 280")
+flavor = input("1or2or3")
+price_flavor = 0
+big = False
+egg = False
+chashu = False
