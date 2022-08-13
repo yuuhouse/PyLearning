2098e48f4610c9805e3da341d894f83eea52d0eb
yuuhouse <44646567+yuuhouse@users.noreply.github.com>
2022-08-13 16:57:00 +0800

    return

--- main19.py
+++ main19.py
@@ -1,12 +1,14 @@
 # 比大小 find max
 
 def find_max(num1, num2, num3):
+    if type(num1)!= int or type(num2) !=int or type(num3) != int:
+        return "請輸入整數"  # return之後下面的程式都不會執行
     if num1 > num2 and num1 > num3:
-        print(num1)
+        return num1
     elif num2 > num1 and num2 > num3:
-        print(num2)
+        return num2
     else:
-        print(num3)
+        return num3
 
 
-find_max(10, 60, 30)
+print(find_max("10", 60, 30))
